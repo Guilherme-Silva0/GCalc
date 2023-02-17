f26d087a8f7f210ee14fe7ae491475c55e305213
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-02-17 15:27:30 -0300

    Criando função para lidar com digito na tela

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Calculadora from "./components/Calculadora";
 
 const App = () => {
+  const [screenValue, setScrenValue] = useState("");
+  const [result, setResult] = useState(0);
+  const [accumulator, setAccumulator] = useState();
+  const [operated, setOperated] = useState(false);
+
+  const HandleAddDigitScreen = (digit) => {
+    if (
+      digit === "+" ||
+      digit === "-" ||
+      digit === "÷" ||
+      (digit === "X" && operated)
+    ) {
+      console.log("+-*/");
+      setOperated(false);
+      setScrenValue(result + digit);
+      return;
+    }
+
+    if (operated) {
+      setScrenValue(digit);
+      setOperated(false);
+      return;
+    }
+
+    setScrenValue(screenValue + digit);
+  };
+
   return <Calculadora />;
 };
 
