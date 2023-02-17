de4e770e35c33e499763462948aefe5ef4c9c016
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-02-17 13:59:41 -0300

    Criando e estilizando componente Calculadora

---
+++ src/components/Calculadora.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Painel from "./Painel";
+import Button from "./Button";
+
+import "./Calculadora.css";
+
+const Calculadora = () => {
+  return (
+    <div className="container">
+      <Painel />
+      <div className="buttons-area">
+        <Button>AC</Button>
+        <Button>(</Button>
+        <Button>)</Button>
+        <Button>÷</Button>
+        <Button>7</Button>
+        <Button>8</Button>
+        <Button>9</Button>
+        <Button>X</Button>
+        <Button>4</Button>
+        <Button>5</Button>
+        <Button>6</Button>
+        <Button>-</Button>
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+        <Button>+</Button>
+        <Button>0</Button>
+        <Button>.</Button>
+        <Button>{"<-"}</Button>
+        <Button>=</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Calculadora;
