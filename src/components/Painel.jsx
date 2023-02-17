28af2ce97b959d4912b149557260dfd98408bb7d
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-02-17 15:05:58 -0300

    Adicionando duas linhas para o painel

--- src/components/Painel.jsx
+++ src/components/Painel.jsx
@@ -3,7 +3,12 @@ import React from "react";
 import "./Painel.css";
 
 const Painel = () => {
-  return <div className="painel">50</div>;
+  return (
+    <div className="painel">
+      <span className="operation">25 + 25</span>
+      <span className="result">50</span>
+    </div>
+  );
 };
 
 export default Painel;
